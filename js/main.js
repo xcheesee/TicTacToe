8fe6688eca370504d23232e169c71f17a6b0fcab
xcheesee <lucas_1219@hotmail.com>
2022-05-14 21:08:56 -0300

    implemented player, board and display methods; rough html page display and css style

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const Player = (name, symbol) => {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    return {getName, getSymbol}
+}
+
+let michael = Player('michael', 'O');
+let john = Player('john', 'X');
+
+const gameBoard = (() => {
+    const square = Array(9).fill(null).map(function(element) {
+        element = {
+            value : ' ',
+            display : document.createElement('div')
+        }
+
+        element.display.classList.add('pogg')
+        element.display.innerHTML = `<p>${element.value}</p>`
+        return element;
+    })
+    return {square};
+})();
+
+const displayController = (() => {
+    const body = document.querySelector('body');
+    const container = document.createElement('div')
+    container.classList.add('container')
+    gameBoard.square.forEach(element => {
+        element.display.addEventListener('click', x => x.target.innerHTML = `<p>${element.value}</p>`)
+        container.appendChild(element.display)
+    })
+    body.appendChild(container)
+})();
