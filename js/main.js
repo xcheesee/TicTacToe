96688e67ba5ce1c8a714cb0e9fd8a2a5cb2ca637
lucasPc <lucas121520@gmail.com>
2022-05-19 20:26:40 -0300

    basic win Check implementation

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ const player = (name, symbol) => {
     return {getName, getSymbol};
 }
 
-let players = [player('michael', 'X'), player('bob', 'O')]
+const players = [player('michael', 'X'), player('bob', 'O')]
 
 const gameBoard = (() => {
     let square = Array.from({length : 9}, function(x) {
         x = {
-            value : 'x',
+            value : null,
             display : document.createElement('div')
         }
         x.display.classList.add('gameSquare')
@@ -31,21 +31,59 @@ const displayController = (() => {
     const body = document.querySelector('body')
 
     body.appendChild(container)
-    gameBoard.square.forEach(element => {
-        container.appendChild(element.display)
-        element.display.addEventListener("click", x => {
+    gameBoard.square.forEach(function(square, squareIndex) {
+        container.appendChild(square.display)
+        console.log(squareIndex)
+        square.display.addEventListener("click", x => {
             if(x.target.innerHTML) return
-            changePlayer();
             x.target.innerHTML = `<p class="squareText">${gameController.currPlayer.getSymbol()}</p>`;
-            checkWinner();
+            square.value = gameController.currPlayer.getSymbol()
+            console.log(checkWinner(gameController.currPlayer, squareIndex))
+            changePlayer();
         })
     })
 })();
 
 function changePlayer () {
     gameController.currPlayer = gameController.currPlayer == players[0]? players[1] : players[0];
+    
 }
 
-function checkWinner () {
+function checkWinner (roundPlayer, squarePlace) {
+    let filledSquares = gameBoard.square
+    if(!filledSquares[1].value && !filledSquares[3].value && !filledSquares[4].value && !filledSquares[5].value && !filledSquares[7].value){
+        return "pog"
+    }
+    if(checkHoriz(gameBoard.square, roundPlayer.getSymbol(), squarePlace) || checkVert(gameBoard.square, roundPlayer.getSymbol(), squarePlace) || checkDiag(gameBoard.square, roundPlayer.getSymbol(), squarePlace)) {
+        return `Congratulations ${roundPlayer.getName()}`
+    }
+}
 
+function checkHoriz(board, symb, index) {
+    if(index < 3) {
+        if(board[index].value == symb && board[index + 3].value == symb && board[index + 6].value == symb) {
+            return true
+        }
+        return false;
+    }
+    else if(index < 6) {
+        if(board[index].value == symb && board[index - 3].value == symb && board[index + 3].value == symb) {
+            return true
+        }
+        return false;
+    }
+    else {
+        if(board[index].value == symb && board[index - 3].value == symb && board[index - 6].value == symb) {
+            return true
+        }
+        return false;
+    }
+}
+
+function checkDiag (board, symb, index) {
+    return false
+}
+
+function checkVert (board, symb, index) {
+    return false
 }
