09f886b83e019b35bf6c74de0c3ef4129ff97da8
xcheesee <lucas_1219@hotmail.com>
2022-05-20 14:34:05 -0300

    fixed issue on game draw not reseting board correctlly

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ const players = (() => {
     const store = player => list.push(player)
     return {list, store}
 })();
+
 players.store(player('michael', 'X'))
 players.store(player('bob', 'O'))
 
@@ -24,19 +25,23 @@ const gameBoard = (() => {
         return x;
     })
     const clean = () => {
-        body.removeChild(document.querySelector('.container'))
+        // body.removeChild(document.querySelector('.container'))
         square.forEach(function (element) {
         element.value = null,
         element.display.innerHTML = ''
+        element.display.classList.remove('winnerLine')
         })
         displayController.drawBoard()
+        gameController.resetPlays()
     }
 
     return {square, clean}
 
 })();
+
 let button = document.querySelector('button')
 button.addEventListener('click', gameBoard.clean)
+
 const gameController = (() => {
     let counter = 0;
     let currPlayer = players.list[0]
@@ -45,33 +50,47 @@ const gameController = (() => {
     }
 
     const getPlays = () => counter;
-    const declareDraw = () => gameBoard.clean()
+    let resetPlays = () => counter = 0;
     const declareWin = (player) => `Congrats ${player}`
 
-    return {currPlayer, play, getPlays, declareDraw, declareWin};
+    return {currPlayer, play, getPlays, resetPlays, declareWin};
 })();
 
 const displayController = (() => {
 
-    const drawBoard = () => {
-        const container = document.createElement('div')
-        container.classList.add('container')
-        body.appendChild(container)
-        gameBoard.square.forEach(function(square, squareIndex) {
-        container.appendChild(square.display)
-        square.display.addEventListener("click", x => {
+    const container = document.createElement('div')
+    container.classList.add('container')
+    body.appendChild(container)
+    gameBoard.square.forEach(function(element, elementIndex) {
+        element.display.addEventListener("click", x => {
             if(x.target.innerHTML) return
             x.target.innerHTML = `<p class="squareText">${gameController.currPlayer.getSymbol()}</p>`;
-            square.value = gameController.currPlayer.getSymbol()
-            console.log(checkWinner(gameController.currPlayer, squareIndex))
-            changePlayer();
+            element.value = gameController.currPlayer.getSymbol()
             gameController.play();
+            if(checkWinner(gameController.currPlayer, elementIndex) == 'random') {gameBoard.clean()}
+            else {
+                changePlayer();
+            }
         })
+    })
+    const drawBoard = () => {
+        gameBoard.square.forEach(function(square) {
+        container.appendChild(square.display)
+        // square.display.addEventListener("click", x => {
+        //     if(x.target.innerHTML) return
+        //     x.target.innerHTML = `<p class="squareText">${gameController.currPlayer.getSymbol()}</p>`;
+        //     square.value = gameController.currPlayer.getSymbol()
+        //     gameController.play();
+        //     if(checkWinner(gameController.currPlayer, squareIndex) == 'random') {gameBoard.clean()}
+        //     else {
+        //         changePlayer();
+        //     }
+        // })
     })}
     return {drawBoard}
 })();
 
-displayController.drawBoard()
+
 function changePlayer () {
     gameController.currPlayer = gameController.currPlayer == players.list[0]? players.list[1] : players.list[0];
     
@@ -79,28 +98,31 @@ function changePlayer () {
 
 function checkWinner (roundPlayer, squarePlace) {
     let filledSquares = gameBoard.square
-    if(!filledSquares[1].value && !filledSquares[3].value && !filledSquares[4].value && !filledSquares[5].value && !filledSquares[7].value){
-        return;
+    if(!filledSquares[1].value && !filledSquares[3].value && !filledSquares[4].value && !filledSquares[5].value && !filledSquares[7].value || gameController.getPlays() < 4){
+        return 'inconclusive';
     } else if(checkHoriz(gameBoard.square, roundPlayer.getSymbol(), squarePlace) || checkVert(gameBoard.square, roundPlayer.getSymbol(), squarePlace) || checkDiag(gameBoard.square, roundPlayer.getSymbol(), squarePlace)) {
         return gameController.declareWin(roundPlayer.getName());
-    } else if(gameController.getPlays() == 8) {
-        return gameController.declareDraw();
+    } else if(gameController.getPlays() > 8) {
+        return 'random';
     }
 }
 
 function checkVert(board, symb, index) {
     if(index < 3) {
         if(board[index].value == symb && board[index + 3].value == symb && board[index + 6].value == symb) {
+            drawWin(index, index + 3, index + 6, board)
             return true
         }
         return false;
     } else if(index < 6) {
         if(board[index].value == symb && board[index - 3].value == symb && board[index + 3].value == symb) {
+            drawWin(index, index - 3, index + 3, board)
             return true
         }
         return false;
     } else {
         if(board[index].value == symb && board[index - 3].value == symb && board[index - 6].value == symb) {
+            drawWin(index, index - 3, index - 6, board)
             return true
         }
         return false;
@@ -110,27 +132,32 @@ function checkVert(board, symb, index) {
 function checkDiag (board, symb, index) {
     if(index == 4) {
         if(board[index].value == symb && board[index + 2].value == symb && board[index - 2].value == symb || board[index].value == symb && board[index + 4].value == symb && board[index - 4].value == symb) {
+            // drawWin(index, index + 4, index + 8, board) need to break the conditionals to correct function
             return true;
         }
         return false;
     }
     else if(index == 0) {
         if(board[index].value == symb && board[index + 4].value == symb && board[index + 8].value == symb) {
+            drawWin(index, index + 4, index + 8, board)
             return true;
         }
         return false;
     } else if(index == 2) {
         if(board[index].value == symb && board[index + 2].value == symb && board[index + 4].value == symb) {
+            drawWin(index, index + 2, index + 4, board)
             return true;
         }
         return false;
     } else if(index == 6) {
         if(board[index].value == symb && board[index - 2].value == symb && board[index - 4].value == symb) {
+            drawWin(index, index - 2, index - 4, board)
             return true;
         }
         return false;
     } else if(index == 8) {
         if(board[index].value == symb && board[index - 4].value == symb && board[index - 8].value == symb) {
+            drawWin(index, index - 4, index - 8, board)
             return true;
         }
         return false;
@@ -140,18 +167,29 @@ function checkDiag (board, symb, index) {
 function checkHoriz (board, symb, index) {
     if(index == 0 || index == 3 || index == 6) {
         if(board[index].value == symb && board[index + 1].value == symb && board[index + 2].value == symb) {
+            drawWin(index, index + 1, index + 2, board)
             return true;
         }
         return false;
     } else if(index == 1 || index == 4 || index == 7) {
         if(board[index].value == symb && board[index + 1].value == symb && board[index - 1].value == symb) {
+            drawWin(index, index + 1, index - 1, board)
             return true;
         }
         return false;
     } else {
         if(board[index].value == symb && board[index - 1].value == symb && board[index - 2].value == symb) {
+            drawWin(index, index - 1, index - 2, board)
             return true;
         }
         return false;
     }
 }
+
+function drawWin(place1, place2, place3, board) {
+    board[place1].display.classList.add('winnerLine')
+    board[place2].display.classList.add('winnerLine')
+    board[place3].display.classList.add('winnerLine')
+}
+
+displayController.drawBoard()
